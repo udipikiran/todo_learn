c3346bb2299b76a51c98e98aeedf4e0353e07da1
udipikiran <udupikirankumar@gmail.com>
2024-11-21 15:56:44 +0530

    Updated with Add Features

---
+++ todo/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from todo.models import Task
+
+
+# Create your views here.
+def addTask(request):
+    task = request.POST['task']
+    Task.objects.create(task=task)
+    return redirect('home')
