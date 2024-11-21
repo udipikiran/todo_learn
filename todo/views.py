230f44c5c6bc36df4f06f49968bf5a08b0132eff
udipikiran <udupikirankumar@gmail.com>
2024-11-21 20:01:08 +0530

    Updated with new features like mark as done and mark as undone

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from todo.models import Task
 
@@ -7,4 +7,16 @@ from todo.models import Task
 def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
     return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,8 +4,10 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
+    complted_tasks = Task.objects.filter(is_completed=True)
     context = {
         'tasks': tasks,
+        'completed_tasks':complted_tasks,
     }
     print(tasks)
     return render(request, 'home.html', context)
